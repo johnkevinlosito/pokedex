6010ae5e8d02eb33427d82c87469a226af21e500
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-08-25 10:59:42 +0800

    Initial setup

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
+
+const Layout = (props) => {
+    return (
+        <>
+            <Header></Header>
+            {props.children}
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default Layout;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+    return <div>Pokemon Homepage</div>;
+};
+
+export default Home;
