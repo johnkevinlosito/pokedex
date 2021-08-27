f3831a0b866103263772303b54706ea88e90d79d
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-08-27 10:30:10 +0800

    Added optional code for fetching pokemon data

--- src/pages/Pokedex/index.jsx
+++ src/pages/Pokedex/index.jsx
@@ -10,17 +10,30 @@ const Pokedex = () => {
     );
 
     useEffect(() => {
-        const fetchPokemonData = async (url) => {
-            const response = await fetch(url);
-            const res = await response.json();
-            setPokemons((prev) => [...prev, res]);
-        };
-        const fetchPokemons = async () => {
-            const response = await fetch(currentUrl);
-            const res = await response.json();
-            res.results.map((pokemon) => fetchPokemonData(pokemon.url));
-        };
-        fetchPokemons();
+        // use this for pagination
+        fetch(currentUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                const results = data.results;
+                const pokemonData = results.map((result) => {
+                    return fetch(result.url).then((response) =>
+                        response.json()
+                    );
+                });
+                return Promise.all(pokemonData);
+            })
+            .then((data) => setPokemons(data));
+        // const fetchPokemonData = async (url) => {
+        //     const response = await fetch(url);
+        //     const res = await response.json();
+        //     setPokemons((prev) => [...prev, res]);
+        // };
+        // const fetchPokemons = async () => {
+        //     const response = await fetch(currentUrl);
+        //     const res = await response.json();
+        //     res.results.map((pokemon) => fetchPokemonData(pokemon.url));
+        // };
+        // fetchPokemons();
         return () => {};
     }, [currentUrl]);
 
