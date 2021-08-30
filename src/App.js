78d698f7e969e2a18b42c3bad748df1e349d0208
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-08-30 11:03:09 +0800

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
-import NotFound from './pages/404';
-import About from './pages/About';
-import Home from './pages/Home';
-import Pokedex from './pages/Pokedex';
 import Header from './components/Header';
+import Loading from './components/Loading';
 
+const About = React.lazy(() => import('./pages/About'))
+const Home = React.lazy(() => import('./pages/Home'))
+const Pokedex = React.lazy(() => import('./pages/Pokedex'))
+const NotFound = React.lazy(() => import('./pages/404'))
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-
-          <Header />
-          <Home />
-        </Route>
-        <Route path='/pokedex'>
-          <Header />
-          <Pokedex />
-        </Route>
-        <Route path='/about'>
-          <Header />
-          <About />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route path='/' exact>
+            <Header />
+            <Home />
+          </Route>
+          <Route path='/pokedex'>
+            <Header />
+            <Pokedex />
+          </Route>
+          <Route path='/about'>
+            <Header />
+            <About />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch></Suspense>
     </Layout>
   );
 }
