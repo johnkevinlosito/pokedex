1dfdb4b394254bc91254bab84133a8ce3a7a73d6
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-08-30 11:51:57 +0800

    Added about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Mew from "../../assets/mew.png";
+import Container from "../../components/Container";
+import classes from "./styles.module.css";
 
 const About = () => {
-    return <div>About</div>;
+    return (
+        <section>
+            <Container className={classes.about}>
+                <img src={Mew} className={classes.banner} alt="" />
+                <div className={classes.content}>
+                    <p>
+                        This is a personal project by{" "}
+                        <b>
+                            <a
+                                href="https://johnkevinlosito.com"
+                                target="_blank"
+                            >
+                                John
+                            </a>
+                        </b>{" "}
+                        on his journey in learning React JS. The layout / UI is
+                        inspired from a Figma community file which can be found{" "}
+                        <a
+                            href="https://www.figma.com/community/file/893705420616737018"
+                            target="_blank"
+                        >
+                            here
+                        </a>{" "}
+                        and is licensed under{" "}
+                        <a
+                            href="https://creativecommons.org/licenses/by/4.0/"
+                            target="_blank"
+                        >
+                            CC BY 4.0
+                        </a>
+                    </p>
+                    <p>
+                        This project fetches its data from{" "}
+                        <a href="https://pokeapi.co/" target="_blank">
+                            https://pokeapi.co/
+                        </a>{" "}
+                        which is a free and open to use consumption-only API.{" "}
+                    </p>
+                </div>
+            </Container>
+        </section>
+    );
 };
 
 export default About;
